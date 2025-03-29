71d3a26791d7d3ad5928d181d4e54e1f153fb17f
Your mamer8 <mahmoud74amer@gmail.com>
2025-03-30 01:23:25 +0200

    Enhance custom menu row with improved padding, shadow effect, and optional arrow icon

--- lib/features/menu/screens/menu_screen.dart
+++ lib/features/menu/screens/menu_screen.dart
@@ -21,164 +21,165 @@ class CustomMenu extends StatelessWidget {
     var cubit = context.read<MenuCubit>();
     return Scaffold(
       body: BlocBuilder<MenuCubit, MenuState>(builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: BlocBuilder<ProfileCubit, ProfileState>(
-              builder: (context, state) {
-            var profileCubit = context.read<ProfileCubit>();
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                20.h.verticalSpace,
-                CustomNetworkImage(
-                    image: profileCubit.loginModel.data?.image ?? '',
-                    isUser: true,
-                    width: 100.w,
-                    height: 100.w,
-                    borderRadius: 50.w),
-                10.h.verticalSpace,
-                Text(
-                  AppConst.isLogged
-                      ? profileCubit.loginModel.data?.name ?? ''
-                      : 'guest'.tr(),
-                  textAlign: TextAlign.center,
-                  style: getBoldStyle(fontSize: 18.sp),
-                ),
-                Text(
-                  profileCubit.loginModel.data?.phone ?? '',
-                  textAlign: TextAlign.center,
-                  style:
-                      getRegularStyle(color: AppColors.black, fontSize: 14.sp),
-                ),
-                30.h.verticalSpace,
-                Expanded(
-                    child: SingleChildScrollView(
-                        child: Column(
-                  children: [
+        return BlocBuilder<ProfileCubit, ProfileState>(
+            builder: (context, state) {
+          var profileCubit = context.read<ProfileCubit>();
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              30.h.verticalSpace,
+              CustomNetworkImage(
+                  image: profileCubit.loginModel.data?.image ?? '',
+                  isUser: true,
+                  width: 100.w,
+                  height: 100.w,
+                  borderRadius: 50.w),
+              10.h.verticalSpace,
+              Text(
+                AppConst.isLogged
+                    ? profileCubit.loginModel.data?.name ?? ''
+                    : 'guest'.tr(),
+                textAlign: TextAlign.center,
+                style: getBoldStyle(fontSize: 18.sp),
+              ),
+              Text(
+                profileCubit.loginModel.data?.phone ?? '',
+                textAlign: TextAlign.center,
+                style: getRegularStyle(color: AppColors.black, fontSize: 14.sp),
+              ),
+              30.h.verticalSpace,
+              Expanded(
+                  child: SingleChildScrollView(
+                      child: Column(
+                children: [
+                  CustmMenuRow(
+                    assetName: ImageAssets.languageIcon,
+                    title: 'language'.tr(),
+                    onTap: () {
+                      Navigator.pushNamed(context, Routes.languagesRoute);
+                    },
+                  ),
+                  if (AppConst.isLogged)
                     CustmMenuRow(
-                      assetName: ImageAssets.languageIcon,
-                      title: 'language'.tr(),
+                      assetName: ImageAssets.addOfferIcon,
+                      title: 'add_offer'.tr(),
                       onTap: () {
-                        Navigator.pushNamed(context, Routes.languagesRoute);
+                        Navigator.pushNamed(context, Routes.addOfferRoute);
                       },
                     ),
-                    if (AppConst.isLogged)
-                      CustmMenuRow(
-                        assetName: ImageAssets.addOfferIcon,
-                        title: 'add_offer'.tr(),
-                        onTap: () {
-                          Navigator.pushNamed(context, Routes.addOfferRoute);
-                        },
-                      ),
-                    if (AppConst.isLogged)
-                      CustmMenuRow(
-                        assetName: ImageAssets.myOffersIcon,
-                        title: 'my_offers'.tr(),
-                        onTap: () {
-                          Navigator.pushNamed(context, Routes.myOffersRoute);
-                        },
-                      ),
-                    if (AppConst.isLogged)
-                      CustmMenuRow(
-                        assetName: ImageAssets.profileIcon,
-                        title: 'profile'.tr(),
-                        onTap: () {
-                          Navigator.pushNamed(context, Routes.profileRoute);
-                        },
-                      ),
-                    if (AppConst.isLogged)
-                      CustmMenuRow(
-                        assetName: ImageAssets.favoriteIcon,
-                        title: 'favorite'.tr(),
-                        onTap: () {
-                          Navigator.pushNamed(context, Routes.favoritesRoute);
-                        },
-                      ),
-                    if (cubit.settingsModel.data?.privacy != null)
-                      CustmMenuRow(
-                        assetName: ImageAssets.privacyPolicyIcon,
-                        title: 'privacy_policy'.tr(),
-                        onTap: () {
-                          Navigator.pushNamed(
-                              context, Routes.privacyPolicyRoute);
-                        },
-                      ),
-                    if (cubit.settingsModel.data?.phone != null)
-                      CustmMenuRow(
-                        assetName: ImageAssets.supportIcon,
-                        title: 'support'.tr(),
-                        onTap: () async {
-                          String whatsappNumber =
-                              cubit.settingsModel.data?.phone ?? '';
-                          String url = 'https://wa.me/$whatsappNumber';
-                          if (!await launchUrl(Uri.parse(url),
-                              mode: LaunchMode.externalApplication)) {
-                            throw Exception('Could not launch $url');
-                          }
-                        },
-                      ),
+                  if (AppConst.isLogged)
                     CustmMenuRow(
-                      assetName: ImageAssets.rateAppIcon,
-                      title: 'rate_app'.tr(),
+                      assetName: ImageAssets.myOffersIcon,
+                      title: 'my_offers'.tr(),
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.myOffersRoute);
+                      },
+                    ),
+                  if (AppConst.isLogged)
+                    CustmMenuRow(
+                      assetName: ImageAssets.profileIcon,
+                      title: 'profile'.tr(),
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.profileRoute);
+                      },
+                    ),
+                  if (AppConst.isLogged)
+                    CustmMenuRow(
+                      assetName: ImageAssets.favoriteIcon,
+                      title: 'favorite'.tr(),
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.favoritesRoute);
+                      },
+                    ),
+                  if (cubit.settingsModel.data?.privacy != null)
+                    CustmMenuRow(
+                      assetName: ImageAssets.privacyPolicyIcon,
+                      title: 'privacy_policy'.tr(),
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.privacyPolicyRoute);
+                      },
+                    ),
+                  if (cubit.settingsModel.data?.phone != null)
+                    CustmMenuRow(
+                      assetName: ImageAssets.supportIcon,
+                      title: 'support'.tr(),
                       onTap: () async {
-                        PackageInfo packageInfo =
-                            await PackageInfo.fromPlatform();
-                        String url = '';
-                        String packageName = packageInfo.packageName;
-
-                        if (Platform.isAndroid) {
-                          url =
-                              "https://play.google.com/store/apps/details?id=$packageName";
-                        } else if (Platform.isIOS) {
-                          url = 'https://apps.apple.com/us/app/$packageName';
-                        }
+                        String whatsappNumber =
+                            cubit.settingsModel.data?.phone ?? '';
+                        String url = 'https://wa.me/$whatsappNumber';
                         if (!await launchUrl(Uri.parse(url),
                             mode: LaunchMode.externalApplication)) {
                           throw Exception('Could not launch $url');
                         }
-                        //!
                       },
+                      withRow: false,
                     ),
+                  CustmMenuRow(
+                    assetName: ImageAssets.rateAppIcon,
+                    title: 'rate_app'.tr(),
+                    onTap: () async {
+                      PackageInfo packageInfo =
+                          await PackageInfo.fromPlatform();
+                      String url = '';
+                      String packageName = packageInfo.packageName;
+
+                      if (Platform.isAndroid) {
+                        url =
+                            "https://play.google.com/store/apps/details?id=$packageName";
+                      } else if (Platform.isIOS) {
+                        url = 'https://apps.apple.com/us/app/$packageName';
+                      }
+                      if (!await launchUrl(Uri.parse(url),
+                          mode: LaunchMode.externalApplication)) {
+                        throw Exception('Could not launch $url');
+                      }
+                      //!
+                    },
+                    withRow: false,
+                  ),
+                  CustmMenuRow(
+                    assetName: ImageAssets.shareAppIcon,
+                    title: 'share_app'.tr(),
+                    onTap: () async {
+                      PackageInfo packageInfo =
+                          await PackageInfo.fromPlatform();
+                      String url = '';
+                      String packageName = packageInfo.packageName;
+                      if (Platform.isAndroid) {
+                        url =
+                            "https://play.google.com/store/apps/details?id=$packageName";
+                      } else if (Platform.isIOS) {
+                        url = 'https://apps.apple.com/us/app/$packageName';
+                      }
+                      await Share.share(url);
+                    },
+                    withRow: false,
+                  ),
+                  if (AppConst.isLogged)
                     CustmMenuRow(
-                      assetName: ImageAssets.shareAppIcon,
-                      title: 'share_app'.tr(),
-                      onTap: () async {
-                        PackageInfo packageInfo =
-                            await PackageInfo.fromPlatform();
-                        String url = '';
-                        String packageName = packageInfo.packageName;
-                        if (Platform.isAndroid) {
-                          url =
-                              "https://play.google.com/store/apps/details?id=$packageName";
-                        } else if (Platform.isIOS) {
-                          url = 'https://apps.apple.com/us/app/$packageName';
-                        }
-                        await Share.share(url);
-                      },
-                    ),
-                    if (AppConst.isLogged)
-                      CustmMenuRow(
-                        assetName: ImageAssets.deleteIcon,
-                        title: 'delete_profile'.tr(),
-                        onTap: () {
-                          context.read<MenuCubit>().deleteAccount(context);
-                        },
-                      ),
-                    CustmMenuRow(
-                      title: !AppConst.isLogged ? 'login'.tr() : 'logout'.tr(),
-                      assetName: ImageAssets.logoutIcon,
+                      assetName: ImageAssets.deleteIcon,
+                      title: 'delete_profile'.tr(),
                       onTap: () {
-                        context.read<MenuCubit>().logout(context);
+                        context.read<MenuCubit>().deleteAccount(context);
                       },
+                      withRow: false,
                     ),
-                  ],
-                ))),
-                20.h.verticalSpace,
-                kToolbarHeight.verticalSpace,
-              ],
-            );
-          }),
-        );
+                  CustmMenuRow(
+                    title: !AppConst.isLogged ? 'login'.tr() : 'logout'.tr(),
+                    assetName: ImageAssets.logoutIcon,
+                    onTap: () {
+                      context.read<MenuCubit>().logout(context);
+                    },
+                    isLast: true,
+                    withRow: false,
+                  ),
+                ],
+              ))),
+              30.h.verticalSpace,
+              kToolbarHeight.verticalSpace,
+            ],
+          );
+        });
       }),
     );
   }
--- lib/features/menu/screens/widgets/custom_menu_row.dart
+++ lib/features/menu/screens/widgets/custom_menu_row.dart
@@ -7,33 +7,57 @@ class CustmMenuRow extends StatelessWidget {
     required this.assetName,
     required this.title,
     this.onTap,
+    this.isLast = false,
+    this.withRow = true,
   });
   final String assetName;
   final String title;
   final void Function()? onTap;
+  final bool isLast;
+  final bool withRow;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 22.h),
+      padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 12.w),
       child: GestureDetector(
         onTap: onTap,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              assetName,
-              width: 25.w,
-              height: 25.w,
-            ),
-            15.w.horizontalSpace,
-            Flexible(
-              child: Text(
-                title,
-                style: getMediumStyle(fontSize: 18.sp),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8.h * 2, horizontal: 21.w),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                blurRadius: 6,
+                offset: Offset(0, 3),
               ),
-            ),
-          ],
+            ],
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                assetName,
+                width: 25.w,
+                height: 25.w,
+              ),
+              15.w.horizontalSpace,
+              Expanded(
+                child: Text(
+                  title,
+                  style: getMediumStyle(fontSize: 18.sp),
+                ),
+              ),
+              if (withRow)
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColors.primary,
+                  size: 18.sp,
+                ),
+            ],
+          ),
         ),
       ),
     );
